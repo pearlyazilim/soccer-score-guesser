55634765482c1754f1097a72799bcf8b503a04c0
pearlyazilim <meobey90@gmail.com>
2024-07-27 21:17:32 +0300

    styles.css Code updated.

---
+++ styles.css
@@ -0,0 +1,16 @@
+// script.js
+document.getElementById('generate-button').addEventListener('click', function() {
+    // Rastgele bir sayı oluştur
+    const min = 0;
+    const max = 5;
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+    // Rastgele oran hesapla (20-50 arası)
+    const minPercentage = 40;
+    const maxPercentage = 90;
+    const randomPercentage = Math.floor(Math.random() * (maxPercentage - minPercentage + 1)) + minPercentage;
+    
+    // Ekrana yazdır
+    document.getElementById('number-display').textContent = `Gol: ${randomNumber}`;
+    document.getElementById('percentage-display').textContent = `Oran: %${randomPercentage}`;
+});
